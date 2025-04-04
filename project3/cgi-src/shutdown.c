86a306e81f49eb5f8487818b08c6c41dbb908ce0
Jacob Littler <159176120+dankpompano@users.noreply.github.com>
2025-04-04 11:12:34 -0400

    Update shutdown.c
    passing shutdown

--- project3/cgi-src/shutdown.c
+++ project3/cgi-src/shutdown.c
@@ -9,19 +9,19 @@ main (void)
 {
   // TODO: Print the following message (eliminate the leading spaces on each
   // line):
-  //    <html>
-  //    <head>
-  //      <title>Success!</title>
-  //    </head>
-  //    <body>
-  //      <p>Server has stopped</p>
-  //    </body>
-  //    </html>
+  //<html>
+  //<head>
+  //  <title>Success!</title>
+  //</head>
+  //<body>
+  //  <p>Server has stopped</p>
+  //</body>
+  //</html>
   // Once this program finishes running, the server should shut itself down.
   // (This shutdown is handled in serve.c, not here.)
   printf ("<html>\n");
   printf("<head>\n  <title>Success!</title>\n</head>\n");
-  printf ("<body>\n <p>Server has stopped</p>\n</body>\n");
+  printf("<body>\n  <p>Server has stopped</p>\n</body>\n");
   printf ("</html>\n");
   return 0;
 }
